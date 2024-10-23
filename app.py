15c048f4db3208332c9f644ebf32d30f19dc9c74
Prashant <plm1535172@gmail.com>
2024-10-23 15:14:20 +0530

    update index route function name as home

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Wel-Come to Flask Project.</h1>"
+def home():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
